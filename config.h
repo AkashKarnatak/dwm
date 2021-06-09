4333d5e9bc85e8a9167ba27e0aecdbce08bdd46b
Akash Karnatak <akashkarnatak.ak47@gmail.com>
2021-06-09 20:03:57 +0530

    Set matplotlib window to floating

--- config.h
+++ config.h
@@ -28,8 +28,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
+	{ "Matplotlib",     NULL,       NULL,       0,            1,           -1 },
 	/* { "firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
