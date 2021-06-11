a9a69497a33f897a0644413c30ccc439b8f23b6e
Akash Karnatak <akashkarnatak.ak47@gmail.com>
2021-06-11 10:26:38 +0530

    Removed floating window rule for GIMP

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class            instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
+	/* { "Gimp",           NULL,       NULL,       0,            1,           -1 }, */
 	{ "Matplotlib",     NULL,       NULL,       0,            1,           -1 },
 	/* { "firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
